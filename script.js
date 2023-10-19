2ce8372146d074c0e7471a2877f34de3f89283c1
Isak Horvath <isakhorvath@gmail.com>
2023-10-19 17:30:03 +0200

    sprogh: sun generation on flags

--- script.js
+++ script.js
@@ -9,15 +9,6 @@ function setElement(id, value) {
     }
 }
 
-function getRandomHexColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
 function getRandomBoolean() {
     return Math.random() >= 0.5;
 }
@@ -25,77 +16,6 @@ function getRandomBoolean() {
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-const colorPalette = ["#FF5733", "#FFC300", "#DAF7A6", "#58D68D", "#3498DB", "#AF7AC5"];
-function getRandomColorFromPalette() {
-    const randomIndex = Math.floor(Math.random() * colorPalette.length);
-    return colorPalette[randomIndex];
-}
-
-function generateFlag() {
-    var flagElement = document.getElementById("flag");
-    flagElement.style.display = "flex";
-    flagElement.style.width = "96px";
-    flagElement.style.height = "64px";
-    flagElement.style.border = "1px solid black";
-    flagElement.style.position = "relative";
-
-    const stripes = getRandomNumber(1, 4);
-
-    for(let i = 0; i < stripes; i++) {
-        var div = document.createElement("div");
-        div.style.backgroundColor = getRandomColorFromPalette();
-        div.style.width = "100%";
-        div.style.height = "100%";
-        flagElement.appendChild(div);
-    }
-
-    flagElement.style.flexDirection = getRandomBoolean() ? "column" : "row";
-
-    if(getRandomBoolean()) {
-        if(getRandomBoolean()) {
-            var crossSize = "100%";
-            var crossColor = getRandomBoolean() ? getRandomColorFromPalette() : getRandomHexColor();
-
-            var horizontalLine = document.createElement("div");
-            horizontalLine.style.backgroundColor = crossColor;
-            horizontalLine.style.width = crossSize;
-            horizontalLine.style.height = "10px";
-            horizontalLine.style.position = "absolute";
-            horizontalLine.style.left = "50%";
-            horizontalLine.style.top = "50%";
-            var perY = getRandomNumber(-100, 100);
-            horizontalLine.style.transform = "translate(-50%, " + perY + "%)";
-            flagElement.appendChild(horizontalLine);
-
-            var verticalLine = document.createElement("div");
-            verticalLine.style.backgroundColor = crossColor;
-            verticalLine.style.width = "10px";
-            verticalLine.style.height = crossSize;
-            verticalLine.style.position = "absolute";
-            verticalLine.style.left = "50%";
-            verticalLine.style.top = "50%";
-            var perX = getRandomNumber(-300, 300);
-            verticalLine.style.transform = "translate(" + perX + "%, -50%)";
-            flagElement.appendChild(verticalLine);
-        }
-        else {
-            var circle = document.createElement("div");
-            circle.style.backgroundColor = getRandomBoolean() ? getRandomColorFromPalette() : getRandomHexColor();
-            circle.style.borderRadius = "50%";
-            var circleSize = getRandomNumber(25, 50);
-            circle.style.width = circleSize + "px";
-            circle.style.height = circleSize + "px";
-            circle.style.position = "absolute";
-            circle.style.left = "50%";
-            circle.style.top = "50%";
-            circle.style.transform = "translate(-50%, -50%)";
-            flagElement.appendChild(circle);
-        }
-    }
-}
-
-generateFlag();
 
 const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "z", "x"];
 const vowles = ["a", "e", "i", "o", "u", "y", "å", "ä", "ö"];
