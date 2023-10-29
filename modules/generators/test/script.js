fe8d351c081cad23883b30da7318df844cc66aec
Isak Horvath <isakhorvath@gmail.com>
2023-10-29 13:56:00 +0100

    sprogh: continue work on abstract relations ds

--- modules/generators/test/script.js
+++ modules/generators/test/script.js
@@ -22,6 +22,31 @@ const jsonData = {
     ]
 };
 
+function generateRandomNames(number, charLength) {
+    const vowels = 'aeiou';
+    const consonants = 'bcdfghjklmnpqrstvwxyz';
+    const names = new Set();
+
+    while (names.size < number) {
+        let name = '';
+        let length = Math.floor(Math.random() * charLength) + charLength;
+        let isVowelStart = Math.random() >= 0.5;
+
+        for (let i = 0; i < length; i++) {
+            if (isVowelStart) {
+                name += vowels[Math.floor(Math.random() * vowels.length)];
+            } else {
+                name += consonants[Math.floor(Math.random() * consonants.length)];
+            }
+            isVowelStart = !isVowelStart;
+        }
+
+        names.add(name.charAt(0).toUpperCase() + name.slice(1));
+    }
+
+    return Array.from(names);
+}
+
 function getAsArr(inputStr, delimiter) {
     return inputStr.split(delimiter).map(str => str.trim());
 }
@@ -48,7 +73,45 @@ function getRandomElement(data) {
     };
 }
 
-const randomData = getRandomElement(jsonData);
+function processAgain(data) {
+    const randomValue = data;
+
+    const elements = randomValue.match(/\[([A-Öa-ö0-9_:]+)\]/g) || [];
+    const relationships = randomValue.match(/\{([^{}]+)\}/g) || [];
+
+    const cleanedElements = elements.map(match => match.replace(/\[|\]/g, ''));
+    let cleanedRelationships = relationships.map(match => match.replace(/\{|\}/g, ''));
+    let rels = getAsArr(cleanedRelationships[0], ',');
+
+    return {
+        value: randomValue,
+        elements: cleanedElements,
+        relationships: rels
+    };
+}
+
+function replaceWordInString(wordToReplace, inputString, newWord) {
+    var regex = new RegExp("\\b" + wordToReplace + "\\b", "gi");
+    var outputString = inputString.replace(regex, newWord);
+    return outputString;
+}
+
+function removeBracketsAndContent(inputString) {
+    let stringWithoutBrackets = inputString.replace(/[\[\]]/g, '');
+    let stringWithoutContent = stringWithoutBrackets.replace(/\{[^}]*\}/g, '');
+    let finalString = stringWithoutContent.replace(/[{}]/g, '');
+    return finalString;
+}
+
+var randomData = getRandomElement(jsonData);
+console.log(randomData);
+for(let i = 0; i < randomData.elements.length; i++) {
+    const name = generateRandomNames(1, 3)[0];
+    randomData.value = replaceWordInString(randomData.elements[i], randomData.value, name);
+}
+randomData = processAgain(randomData.value);
+randomData.value = removeBracketsAndContent(randomData.value);
+console.log(randomData);
 
 const graph = new RelationGraph();
 randomData.elements.forEach(element => {
